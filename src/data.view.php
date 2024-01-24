c6d5c70744fb68a455fef7a57efc39d2c5b99ac2
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-01-24 17:36:33 -0600

    refactored home page

---
+++ src/data.view.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = 'Samir Mahmoud';
+
+$personalGitHub = 'https://github.com/SaMoBTW';
+
+
+
+$navArray = [
+    'Home',
+    'Assignments',
+    'About Me',
+    'Personal Projects',
+    'CS3270 Advanced Web Programming',
+];
+
+
+
+
+?>
---
+++ src/head.view.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <title>Samir Mahmoud's page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href='https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css' rel='stylesheet'>
+  <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  <link rel="stylesheet" href="styles.css">
+  <link rel="icon" href="images/duckfavicon.png">
+</head>
