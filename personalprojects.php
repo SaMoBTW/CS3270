ad5fe25fc8725221b2144062780bb62608cd0f62
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-03-01 19:03:47 -0600

    put up some projects

--- personalprojects.php
+++ personalprojects.php
@@ -20,28 +20,84 @@ include_once 'src/head.view.php';
     <?php include_once 'src/navbar.view.php'; ?>
     <!--Container Main start-->
     <main>
+    <div class="row">
 
-        <div class="card" style="width: 18rem;">
-          <img src="images/tindogss.png" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">TinDog</h5>
-            <p class="card-text">Simple webpage made with bootstrap</p>
-            <p class="card-text">HTML, CSS, BootStrap</p>
-            <div class="container">
-                <div class="row">
-                        <div class="col text-center">
-            <a href="https://samobtw.github.io/Tindog/" target="_blank" class="btn btn-primary card-btn"><i class='bx bxl-github nav_icon'></i></a>
-            <a href="https://samobtw.github.io/Tindog/" target="_blank" class="btn btn-primary card-btn"><i class='bx bx-desktop nav_icon'></i></a>
-                        </div>
-                    </div>
+        <div class="col-md-6">
+            <!-- First card -->
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/tindogss.png" class="card-img-top" alt="...">
                 </div>
-          </div>
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0 gap card-title">Tindog</h3>
+                    <p class="mb-1 text-muted gap left-margin">HTML, CSS</p>
+                    <h5 class="card-text mb-auto gap left-margin">Just a simple single webpage made using Bootstrap.</h5>
+                    <a href="https://github.com/SaMoBTW/tindog" target="_blank" class="stretched-link btn-primary gap left-margin purple-text">Github Repo</a>
+                </div>
+            </div>
         </div>
 
-        
+        <div class="col-md-6">
+            <!-- Second card -->
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/simon.png" class="card-img-top" alt="...">
+                </div>
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0 gap card-title">Simon</h3>
+                    <p class="mb-1 text-muted gap left-margin">HTML, CSS, JavaScript</p>
+                    <h5 class="card-text mb-auto gap left-margin">A memory game based on the children's toy "Simon"</h5>
+                    <a href="https://github.com/SaMoBTW/Simon-Game" target="_blank" class="stretched-link btn-primary gap left-margin purple-text">Github Repo</a>
+                </div>
+            </div>
+        </div>
 
-        
+                <div class="col-md-6">
+            <!-- Third card -->
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/simon.png" class="card-img-top" alt="...">
+                </div>
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0 gap card-title">Simon</h3>
+                    <p class="mb-1 text-muted gap left-margin">HTML, CSS, JavaScript</p>
+                    <h5 class="card-text mb-auto gap left-margin">Another project description.</h5>
+                    <a href="https://github.com/SaMoBTW/Simon-Game" target="_blank" class="stretched-link btn-primary gap left-margin purple-text">Github Repo</a>
+                </div>
+            </div>
+        </div>
 
+                <div class="col-md-6">
+            <!-- fourth card -->
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/simon.png" class="card-img-top" alt="...">
+                </div>
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0 gap card-title">Simon</h3>
+                    <p class="mb-1 text-muted gap left-margin">HTML, CSS, JavaScript</p>
+                    <h5 class="card-text mb-auto gap left-margin">Another project description.</h5>
+                    <a href="https://github.com/SaMoBTW/Simon-Game" target="_blank" class="stretched-link btn-primary gap left-margin purple-text">Github Repo</a>
+                </div>
+            </div>
+        </div>
+
+                <div class="col-md-6">
+            <!-- Fifth card -->
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col-auto d-none d-lg-block">
+                    <img src="images/simon.png" class="card-img-top" alt="...">
+                </div>
+                <div class="col p-4 d-flex flex-column position-static">
+                    <h3 class="mb-0 gap card-title">Simon</h3>
+                    <p class="mb-1 text-muted gap left-margin">HTML, CSS, JavaScript</p>
+                    <h5 class="card-text mb-auto gap left-margin">Another project description.</h5>
+                    <a href="https://github.com/SaMoBTW/Simon-Game" target="_blank" class="stretched-link btn-primary gap left-margin purple-text">Github Repo</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
     </main>
     <!--Container Main end-->
     
