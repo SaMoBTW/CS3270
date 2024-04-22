5463a81390cab741ae736a11127878688ea246dc
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-04-22 18:19:18 -0500

    can create posts

--- home.php
+++ home.php
@@ -3,8 +3,8 @@ session_start();
 
 // Check if the user is not logged in, redirect to the login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: admin.php");
-    exit;
+  header("location: admin.php");
+  exit;
 }
 ?>
 
@@ -36,6 +36,78 @@ include_once 'src/head.view.php';
 
         <h1>Home Page</h1>
         <p><a href="admin.php">Logout</a></p>
+        
+ <div class="container">
+    <div class="row">
+      <!-- Create Form -->
+      <div class="col">
+        <form method="post" action="create.post.php">
+          <h3 class="text-center">Create Post</h3>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Project Title</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="title">
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Languages</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="languages">
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
+          </div>
+
+          <div class="mb-3">
+            <div class="input-group">
+              <span class="input-group-text" id="basic-addon3">Project URL</span>
+              <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4" name="project-url">
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <input class="form-control" type="file" id="formFile" name="project-image">
+          </div>
+
+          <button type="submit" class="btn btn-primary" name="submit">Post</button>
+        </form>
+      </div>
+
+      <!-- Delete Form -->
+      <div class="col">
+        <form>
+          <h3 class="text-center">Delete Post</h3>
+          <div class="mb-3">
+            <label for="exampleInputEmail2" class="form-label">PLACEHOLDER</label>
+            <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp">
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputEmail2" class="form-label">PLACEHOLDER</label>
+            <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp">
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea2" class="form-label">PLACEHOLDER</label>
+            <textarea class="form-control" id="exampleFormControlTextarea2" rows="3"></textarea>
+          </div>
+
+          <div class="mb-3">
+            <div class="input-group">
+              <span class="input-group-text" id="basic-addon4">PLACEHOLDER</span>
+              <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3 basic-addon4">
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <input class="form-control" type="file" id="formFile">
+          </div>
+
+          <button type="submit" class="btn btn-primary">Delete</button>
+        </form>
+      </div>
+    </div>
+  </div>
 
     </main>
     <!--Content Container end-->
