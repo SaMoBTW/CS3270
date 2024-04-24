5f54b7f8d0ca06229ac3ad4e8e6aca6f7ed24a07
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-04-24 12:33:27 -0500

    t

--- src/create.post.php
+++ src/create.post.php
@@ -10,18 +10,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 }
 
-$DB_NAME = "smahmoud";
-$DB_HOST = "localhost";
-$DB_USER = "smahmoud";
-$DB_PASS = "ny7015eq"; 
-
-try {
-    $conn = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connecteed Successfully", PHP_EOL;
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+// $DB_NAME = "smahmoud";
+// $DB_HOST = "localhost";
+// $DB_USER = "smahmoud";
+// $DB_PASS = "ny7015eq"; 
 
 $query = "INSERT INTO Projects (Title, Languages, Description, Image, link)VALUES ('$title', '$languages', '$description', '$image', '$projectUrl');";
 
