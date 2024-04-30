ad7c01b7223eb79f5aa46b10cfbbab727e200a52
Samir Mahmoud <84868862+SaMoBTW@users.noreply.github.com>
2024-04-30 14:37:39 -0500

    reworked about page

---
+++ src/newaboutme.view.php
@@ -0,0 +1,136 @@
+        <section class="about-me-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 mb-4 mb-sm-5">
+                <div class="card card-style1 border-0">
+                    <div class="card-body p-1-9 p-sm-2-3 p-md-6 p-lg-7">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 mb-4 mb-lg-0">
+                                <img src="images/PFP.jpg" alt="..." width="400" height="400" class="rounded">
+                            </div>
+                            <div class="col-lg-6 px-xl-10">
+                                <div class="d-lg-inline-block py-1-9 px-1-9 px-sm-6 mb-1-9 rounded name-wrapper">
+                                    <h3 class="h2 text-white mb-0"><?php echo $name?></h3>
+                                    <span class="text-primary2">Web Developer</span>
+                                </div>
+                                <ul class="list-unstyled mb-1-9">
+                                    <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Position:</span> Web developer</li>
+                                    <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Experience:</span> 3 Months</li>
+                                    <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Email:</span> samir.mahmoud@go.minnstate.edu</li>
+                                    <li class="mb-2 mb-xl-3 display-28"><span class="display-26 text-secondary me-2 font-weight-600">Website:<a href="https://cs.bemidjistate.edu/ny7015eq/"></span> www.cs.bemidjistate.edu/ny7015eq</li></a>
+                                    <li class="display-28"><span class="display-26 text-secondary me-2 font-weight-600">Phone:</span> 218 - 508 - 2743</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 mb-4 mb-sm-5">
+                <div>
+                    <span class="section-title text-primary">About Me</span>
+                            <p>
+                                My name is Samir Mohamed Ahmed Mahmoud Khalil. I know, it’s long. I’m from
+                                Sudan, North Africa, but I grew up in Saudi
+                                Arabia for most of my life. I was lucky enough to go to a private school, so I
+                                speak English and Arabic. However, I
+                                consider English my first language.
+                            </p>
+                            <p>
+                                I enrolled in the University of Technology Bahrain in 2019 as a Business
+                                Administration Major. I lacked the confidence
+                                in myself to do computer science, despite my obsession with technology since a
+                                young age.
+                            </p>
+                            <p>
+                                About a year and a half in, I switched majors to Computer Information Systems to
+                                bridge a middle ground between what I
+                                actually want to do and to avoid losing too many credits. I plan on becoming a
+                                web developer, so I enrolled in a Udemy
+                                course. I have been slowly working through it for the past few months and plan
+                                on consistently growing my experience in
+                                this field.
+                            </p>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-12 mb-4 mb-sm-5">
+                        <div class="mb-4 mb-sm-5">
+                            <span class="section-title text-primary about-me-titles">Skill</span>
+                            <div class="progress-text">
+                                <div class="row">
+                                    <div class="col-6">
+                                      <ul><li>HTML/CSS, Javascript, PHP, Node.js</li></ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="progress-text">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <ul><li>Python</li></ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="progress-text">
+                                <div class="row">
+                                    <div class="col-6">
+                                      <ul><li>Video Editing (Adobe Premiere Pro)</li></ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="progress-text">
+                                <div class="row">
+                                    <div class="col-6">
+                                      <ul><li>GFX (Adobe Photoshop, Canva)</li></ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                        <div>
+                            <span class="section-title text-primary">Education</span>
+<div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+        Bemidji State University
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+      <div class="accordion-body">
+        <strong>Computer Information Systems</strong>
+        <ul>
+          <li>GPA: 3.00/4.00</li>
+          <li>Related Coursework: Data Structures & Algorithms, Objects & Design, Object-Oriented Programming, Business
+application development</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        University of Technology Bahrain
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <strong>Business Informatics</strong>
+        <ul>
+          <li>Concentrations: Business Management, Business Applications</li>
+          <li>GPA: 3.48/4.00</li>
+          <li>Related Coursework: Marketing plans, Financial Management, Financial and managerial Accounting, Business
+Information system operation.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
